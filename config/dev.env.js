a82c31d057690ac66494b6b90f297989a42a1053
haohao <73471452@qq.com>
2019-12-16 00:43:07 +0800

    tp_xianxiapai首页

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,5 +2,6 @@ var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  API_BASE_URL:'"http://127.0.0.1/tp_xianxiapai/api/v1"'
 })
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,34 @@
-function formatNumber (n) {
+/* global getCurrentPages */
+export function formatNumber (n) {
   const str = n.toString()
   return str[1] ? str : `0${str}`
 }
+/* 获取当前页url */
+export function getCurrentPageUrl () {
+  var pages = getCurrentPages() // 获取加载的页面
+  console.log(pages)
+  var currentPage = pages[pages.length - 1] // 获取当前页面的对象
+  var url = currentPage.route // 当前页面url
+  return url
+}
 
+/* 获取当前页带参数的url */
+export function getCurrentPageUrlWithArgs () {
+  var pages = getCurrentPages() // 获取加载的页面
+  var currentPage = pages[pages.length - 1] // 获取当前页面的对象
+  var url = currentPage.route // 当前页面url
+  var options = currentPage.options // 如果要获取url中所带的参数可以查看options
+
+  // 拼接url的参数
+  var urlWithArgs = url + '?'
+  for (var key in options) {
+    var value = options[key]
+    urlWithArgs += key + '=' + value + '&'
+  }
+  urlWithArgs = urlWithArgs.substring(0, urlWithArgs.length - 1)
+
+  return urlWithArgs
+}
 export function formatTime (date) {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
@@ -20,5 +46,7 @@ export function formatTime (date) {
 
 export default {
   formatNumber,
-  formatTime
+  formatTime,
+  getCurrentPageUrl,
+  getCurrentPageUrlWithArgs
 }
