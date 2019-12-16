d86458028c214cf2a3aec704c637ecc2e67bda6f
haohao <734714852@qq.com>
2019-12-16 12:29:50 +0800

    20191216

--- src/pages/logs/main.js
+++ src/pages/logs/main.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2019-12-03 18:35:21
- * @LastEditTime: 2019-12-03 19:02:34
+ * @LastEditTime: 2019-12-04 09:37:51
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \xianxiapai\src\pages\logs\main.js
