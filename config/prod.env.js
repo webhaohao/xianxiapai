da27c973c87b3d060d26cefb1b401b8ded41e8f1
haohao <734714852@qq.com>
2020-01-10 17:36:37 +0800

    prod

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2019-12-03 18:35:21
+ * @LastEditTime : 2020-01-10 17:33:50
+ * @LastEditors  : Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \xianxiapai\config\prod.env.js
+ */
 module.exports = {
   NODE_ENV: '"production"'
 }
