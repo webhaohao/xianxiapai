7369d70a20bf908d1156f1d4c602f447ce0e62bc
haohao <734714852@qq.com>
2020-01-13 09:38:45 +0800

    prodaip

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2019-12-03 18:35:21
- * @LastEditTime : 2020-01-10 17:33:50
+ * @LastEditTime : 2020-01-10 17:48:08
  * @LastEditors  : Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \xianxiapai\config\prod.env.js
